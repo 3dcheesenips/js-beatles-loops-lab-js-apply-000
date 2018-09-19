40b625a58d4a09d3546811ea22b5650cfa3e6253
Maharshee roy <m.roy@optonline.net>
2018-09-19 18:12:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,3 +16,11 @@ function johnLennonFacts(array) {
   }
  return results;
  }
+ function iLoveTheBeatles(num) {
+   var array = []
+   do {
+     array.push("I love the Beatles!")
+     num++
+   } while (num <15);
+   return array
+ }
