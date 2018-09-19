bd1025f9894a2ddd0bad5d9fc544d9d2ee6dff3d
Maharshee roy <m.roy@optonline.net>
2018-09-19 18:04:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 // add solution here
-function theBeatlesPlay(musicians, instruments) {
-  var string = [];
-  for (e=0; e<musicians.length; e++) {
-    string[e] = "";
-  }
-  for (i=0; i<musicians.length; i++) {
-    string[i]+= musicians[i] + ' plays ' + instruments[i] ; 
-  }
-  return string
-}
-theBeatlesPlay(["Lucy","sky"],["Hey","jude"])
+ function theBeatlesPlay(musicians, instruments) {
+	var string = [];
+	
+
+ 	for (e=0; e<musicians.length; e++) {
+ 	string[e] = "";
+ 		}
+
+	for (i=0; i<musicians.length; i++) {
+ 		string[i] += musicians[i] + " plays " + instruments[i] ;
+ 	}
+	
+ return string;
+ }
